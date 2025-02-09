e55bbcf2098ba011935286ee7dcf4f72ae1b202f
Shaifali07 <56667719+Shaifali07@users.noreply.github.com>
2025-02-09 18:49:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+       // callAPI function that takes the base and exponent numbers as parameters
+        var callAPI = (base,exponent)=>{
+            // instantiate a headers object
+            var myHeaders = new Headers();
+            // add content type header to object
+            myHeaders.append("Content-Type", "application/json");
+            // using built in JSON utility package turn object to string and store in a variable
+            var raw = JSON.stringify({"base":base,"exponent":exponent});
+            // create a JSON object with parameters for API call and store in a variable
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: raw,
+                redirect: 'follow'
+            };
+            // make API call with parameters and use promises to get response
+            fetch("https://onx2acqnb9.execute-api.ap-south-1.amazonaws.com/dev", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+        // Parsing the response and updating the HTML element with ID 'result'
+        var parsedResult = JSON.parse(result).body;
+        document.getElementById("result").innerHTML = "Result: " + parsedResult;
+    })
+    .catch(error => console.log('error', error));
+        }
+  
